d6b44e4c37e5bb52d60141546628eda1373cdd00
Douglas Kosvoski <douglas.contactpro@gmail.com>
2021-12-03 10:50:23 -0300

    client and server udp

--- src/main.c
+++ src/main.c
@@ -1,5 +1,11 @@
+// printf
 #include "stdio.h"
+// atoi
 #include "stdlib.h"
+// pid_t
+#include "sys/types.h"
+// getpid()
+#include "unistd.h"
 
 // My custom functions
 #include "load_config.h"
@@ -9,8 +15,22 @@
 #define roteador_cfg "./cfg/roteador.config"
 
 int main(int argc, char const *argv[]) {
+
+  // Id passed via argument on execution
+  pid_t rot_id = atoi(argv[1]);
+  // Get own process id
+  pid_t process_id = getpid();
+  // Get parent process id
+  pid_t p_process_id = getppid();
+
+  printf("Router  id: %5d\n", rot_id);
+  printf("Process id: %5d\n", process_id);
+  printf("Parent pid: %5d\n", p_process_id);
+
+
   parse_file(enlaces_cfg);
   parse_file(roteador_cfg);
+
   printf("\n*** Fim do programa ***\n");
   return 0;
 };
