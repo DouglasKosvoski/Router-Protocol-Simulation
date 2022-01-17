028bded21d62835a6b63572de14bf5d1f84a3d5a
Douglas Kosvoski <douglas.contactpro@gmail.com>
2022-01-17 07:04:03 -0300

    set port and ip addr from cfg

--- src/load_config.h
+++ src/load_config.h
@@ -9,18 +9,18 @@ int startswith(const char *original, const char *sufix) {
 }
 
 // Responsible for spliting a buffer at a delimiter and converting char[] into an integer
-void parse_line(char *buff, const char delim[1], int *ori, int *dest, int *cost) {
+void parse_line(char *buff, const char delim[1], char *ori, char *dest, char *cost) {
   char *token = strtok(buff, delim);
   int counter = 0;
   while(token != NULL) {
     if (counter == 0) {
-      *ori = atoi(token);
+      strcpy(ori, token);
     }
     else if (counter == 1) {
-      *dest = atoi(token);
+      strcpy(dest, token);
     }
     else if (counter == 2) {
-      *cost = atoi(token);
+      strcpy(cost, token);
     }
     counter++;
     token = strtok(NULL, delim);
@@ -28,7 +28,7 @@ void parse_line(char *buff, const char delim[1], int *ori, int *dest, int *cost)
 }
 
 // Reads file line-by-line
-void parse_file(char filename[]) {
+void parse_file(char filename[], int rid) {
   FILE* filePointer;
   int buffer_size = 255;
   char buffer[buffer_size];
@@ -38,12 +38,47 @@ void parse_file(char filename[]) {
   printf("\nReading file: %s\n", filename);
   while(fgets(buffer, buffer_size, filePointer)) {
     // check if line contains valid `id, id, cost` format
-    int origem = -1, destino = -1, custo = -1;
+    char ori[20], dest[20], cost[20];
+
     if (!startswith(buffer, "//") && !startswith(buffer, "\n")) {
       // split buffer at delimiter and convert to int
-      parse_line(buffer, " ", &origem, &destino, &custo);
-      printf("%d %d %d\n", origem, destino, custo);
+      parse_line(buffer, " ", ori, dest, cost);
+      if (atoi(ori) == rid || atoi(dest) == rid) {
+        printf("%s %s %s", ori, dest, cost);
+      }
     }
   }
   fclose(filePointer);
+}
+
+void parse_router_config(char filename[], int rid, int *port, char ip[20]) {
+  FILE* filePointer;
+  int buffer_size = 255;
+  char buffer[buffer_size];
+  filePointer = fopen(filename, "r");
+
+  // Loops through the lines
+  printf("\nReading file: %s\n", filename);
+  printf("Searching for router #%d...\n", rid);
+  int found = 0;
+
+  while(fgets(buffer, buffer_size, filePointer)) {
+    char id[20], temp_port[20], temp_ip[20];
+
+    if (!startswith(buffer, "//") && !startswith(buffer, "\n")) {
+      // split buffer at delimiter and convert to int
+      parse_line(buffer, " ", id, temp_port, temp_ip);
+      if (atoi(id) == rid) {
+        *port = atoi(temp_port);
+        strcpy(ip, temp_ip);
+        printf("Found... port: %d ip: %d\n", atoi(temp_port), atoi(temp_ip));
+        found = 1;
+        break;
+      }
+    }
+  }
+  fclose(filePointer);
+  if (found == 0) {
+    printf("No data found for rounter `%d`\n", rid);
+  }
 }
--- src/main.c
+++ src/main.c
@@ -14,22 +14,41 @@
 #define enlaces_cfg "./cfg/enlaces.config"
 #define roteador_cfg "./cfg/roteador.config"
 
+typedef struct router {
+  pid_t id;
+  pid_t process_id;
+  pid_t parent_id;
+  int port;
+  char ip[20];
+} Router;
+
 int main(int argc, char const *argv[]) {
+  if (argc < 2) {
+    printf("Router Id expected !!!\n");
+    return -1;
+  }
 
+  Router *r1 = malloc(sizeof(Router));
   // Id passed via argument on execution
-  pid_t rot_id = atoi(argv[1]);
+  r1->id = atoi(argv[1]);
   // Get own process id
-  pid_t process_id = getpid();
+  r1->process_id = getpid();
   // Get parent process id
-  pid_t p_process_id = getppid();
-
-  printf("Router  id: %5d\n", rot_id);
-  printf("Process id: %5d\n", process_id);
-  printf("Parent pid: %5d\n", p_process_id);
+  r1->parent_id = getppid();
 
 
-  parse_file(enlaces_cfg);
-  parse_file(roteador_cfg);
+  // set port and ip
+  parse_router_config(roteador_cfg, r1->id, &r1->port, r1->ip);
+
+  printf("Router  id: %10d\n", r1->id);
+  printf("Process id: %10d\n", r1->process_id);
+  printf("Parent pid: %10d\n", r1->parent_id);
+  printf("Port      : %10d\n", r1->port);
+  printf("Ip addr   :  %s\n",  r1->ip);
+
+
+  // parse_file(enlaces_cfg, r1->id);
+  // parse_file(roteador_cfg, r1->id);
 
   printf("\n*** Fim do programa ***\n");
   return 0;
